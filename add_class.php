d916104b2d8e3df5880be4daf754a2d8e18ef407
PriyankaKumari <33878807+Pk21452@users.noreply.github.com>
2023-03-17 11:58:12 +0530

    add classes

--- add_class.php
+++ add_class.php
@@ -1,7 +1,7 @@
 						<!-- block -->
                         <div class="block">
                             <div class="navbar navbar-inner block-header">
-                                <div id="" class="muted pull-left"><h4><i class="icon-plus-sign"></i> Add class</h4></div>
+                                <div id="" class="muted pull-left"><h4><i class="icon-plus-sign"></i> Add classes</h4></div>
                             </div>
                             <div class="block-content collapse in">
                                 <div class="span12">
